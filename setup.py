5d185960bf653a37571a442455da1ebeeef71efd
Alexander Korotkov <eiros@me.com>
2023-04-04 23:51:17 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(include=['ble2mqtt', 'ble2mqtt.*']),
     install_requires=[
         'aio-mqtt>=0.2.0',
-        'bleak>=0.12.0',
+        'bleak==0.13.0',
     ],
     classifiers=[
         'Programming Language :: Python :: 3.5',
